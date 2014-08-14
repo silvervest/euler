3609473e1b9a26bd10f72bfcd2da50c69c828e4a
Nick Silvestro <nick.silvestro@vivant.com.au>
2014-08-14 16:56:27 +1000

    cleanup, brute method for problem 8

--- 0008/0008.c
+++ 0008/0008.c
@@ -31,41 +31,67 @@ Find the thirteen adjacent digits in the 1000-digit number that have the greates
 #include <string.h>
 #include <stdlib.h>
 
+unsigned long greatest = 0;
 char number[] = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
 
+void brute(char *token) {
+    int i, j;
+    unsigned long product;
+
+    for (i = 0; i < strlen(token) - 12; i++) {
+        product = 1;
+        for (j = 0; j < 13; j++) {
+            printf("%c, ", token[i+j]);
+            product *= token[i+j] - '0';
+        }
+
+        printf("product: %lu\n", product);
+        if (product > greatest) {
+            greatest = product;
+        }
+    }
+}
+
+void magic(char *token) {
+    int i = 0, j;
+    char *start = token, *end = start;
+    unsigned long product = 1;
+
+    /* this works out the product using a rolling 13 character window */
+    while (*end) {
+        /* only after we've multiplied 13 digits should we start moving the window */
+        if (i >= 13) {
+            /* divide the product by the value on the "front" of the window */
+            product /= *start++ - '0';
+        }
+
+        /* multiply the product by the value on the "end" of the window */
+        product *= *end++ - '0'; 
+
+        /* just pretty printing */
+        for (j = 0; j < 13; j++) {
+            printf("%c, ", start[j]);
+        }
+        printf("product: %lu\n", product);
+
+        if (product > greatest) {
+            greatest = product;
+        }
+
+        i++;
+    }
+}
+
 int main(void) {
-    unsigned long greatest = 0;
     /* since products with 0 in them are useless, let's break this number apart and work on the smaller values */
     char *token = strtok(number, "0");
 
     while (token) {
         /* don't use tokens that are less than 13 numerals long */
         if (strlen(token) >= 13) {
-            int i = 0;
-            char *start = token, *end = start;
-            unsigned long product = 1;
-
             printf("got token: %s\n", token);
-
-            /* this works out the product using a rolling 13 character window */
-            while (*end) {
-                /* multiply the product by the value on the "end" of the window */
-                product *= *end++ - '0'; /* subtract '0' because the ptr value is the ascii code and this cheat-casts it back to int value */
-
-                /* only after we've multiplied 13 digits should we start using these values */
-                if (i >= 13) {
-                    /* divide the product by the value on the "front" of the window */
-                    product /= *start++ - '0';
-    
-                    printf("product: %lu\n", product);
-
-                    if (product > greatest) {
-                        greatest = product;
-                    }
-                }
-
-                i++;
-            }
+            brute(token);
+            //magic(token);
         }
         token = strtok(NULL, "0");
     }
